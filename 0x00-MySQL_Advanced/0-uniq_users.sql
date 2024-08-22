028b45c2c4487917574318116e6cffab3408a31a
IbnuJabir <kedirjabir12@gmail.com>
2024-08-22 11:10:06 +0300

    add a comment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,10 @@
+-- Task: Create a table named 'users' with specific attributes, ensuring 'email' is unique.
+-- This script will run on Ubuntu 18.04 LTS using MySQL 5.7 (version 5.7.30).
+
+/* Creating the 'users' table if it doesn't already exist */
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,      -- 'id' is the primary key, auto-incremented, and never null
+    email VARCHAR(255) NOT NULL UNIQUE,  -- 'email' is a string, never null, and must be unique
+    name VARCHAR(255),                   -- 'name' is a string with a maximum length of 255 characters
+    PRIMARY KEY (id)                     -- Setting 'id' as the primary key
 );
