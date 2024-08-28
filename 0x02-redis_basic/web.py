82470a79892228a8dbb31c004f70e938b7f5e904
IbnuJabir <kedirjabir12@gmail.com>
2024-08-29 00:21:16 +0300

    implmentation for expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python3
 """ Redis Module """
 
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
+
+redis_ = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """Decorator to count how many times a URL is accessed."""
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url: str) -> str:
-        r = redis.Redis()
-        count_key = f"count:{url}"
-        r.incr(count_key)
-        return method(url)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    """Fetches the content of the URL and caches it with an expiration."""
-    r = redis.Redis()
-    cache_key = f"cache:{url}"
-    cached_content = r.get(cache_key)
-
-    if cached_content:
-        return cached_content.decode('utf-8')
-
-    response = requests.get(url)
-    content = response.text
-
-    # Cache the result with an expiration of 10 seconds
-    r.setex(cache_key, 10, content)
-
-    return content
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
